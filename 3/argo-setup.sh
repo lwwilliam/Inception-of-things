eb99b31a049e5c28e1f4fed199ebc355164772d4
William <73087063+lwwilliam@users.noreply.github.com>
2024-10-01 16:12:14 +0800

    Create argo-setup.sh

---
+++ 3/argo-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo kubectl create namespace argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
