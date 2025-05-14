3de399f46602c03c0fc68ce603b441bc75a5ed0c
lwwilliam <lee1052william@gmail.com>
2025-05-14 17:06:54 +0800

    argocd

--- p3/argo-setup.sh
+++ p3/argo-setup.sh
@@ -4,23 +4,21 @@ k3d cluster create mycluster
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
-
 kubectl create namespace dev
-kubectl apply -f deployment.yaml -n dev
-kubectl port-forward service/app -n dev 8888:8888 > /dev/null 2>&1 &
+kubectl apply -f proj.yaml -n argocd
+kubectl apply -f app.yaml -n argocd
 
-kubectl config set-context --current --namespace=argocd
-argocd app create dev-app --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace dev
-
-argocd app set dev-app --sync-policy automated
-
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
+kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
+kubectl wait --for=condition=Ready pods --all -n dev --timeout=300s
+kubectl port-forward svc/will42 -n dev 8888:8888 > /dev/null 2>&1 &
 
 ## Get ArgoCD Default password ##
-# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
