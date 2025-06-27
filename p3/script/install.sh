540613b334b69f1299173645d80a62846f1156e9
lwwilliam <lee1052william@gmail.com>
2025-06-27 17:31:01 +0800

    debian install

--- p3/script/install.sh
+++ p3/script/install.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+set -e
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y ca-certificates curl lsb-release gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+CODENAME=$(lsb_release -cs)
+
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $CODENAME stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo groupadd -f docker
+sudo usermod -aG docker "$USER"
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
